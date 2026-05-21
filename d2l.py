3ea0a9e5c2783d4d334d1fd5276c394c269c6099
Haokun.Feng <haokun.fung@gmail.com>
2026-05-20 23:32:29 -0400

    update d2l.py

--- d2l.py
+++ d2l.py
@@ -183,6 +183,16 @@ def linreg(X, w, b): #@save
     """线性回归模型"""
     return torch.matmul(X, w) + b
 
+def softmax(X):
+    """softmax函数"""
+    X_exp = torch.exp(X)
+    partition=  X_exp.sum(1, keepdim=True)
+    return X_exp / partition
+
+def softmax_net(X, W, b): #@save
+    """softmax回归模型"""
+    return softmax(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
+
 
 # loss
 # --------------------------------------------------------------------------
@@ -190,6 +200,9 @@ def squared_loss(y_hat, y): #@save
     """均方损失"""
     return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
 
+def cross_entropy(y_hat, y): #@save
+    """交叉熵损失"""
+    return - torch.log(y_hat[range(len(y_hat)), y])
 
 # optimization
 # --------------------------------------------------------------------------
@@ -199,3 +212,62 @@ def sgd(params, lr, batch_size):
         for param in params:
             param -= lr * param.grad / batch_size
             param.grad.zero_()
+
+# accuracy
+# --------------------------------------------------------------------------
+def accuracy(y_hat, y): #@save
+    """计算预测正确的数量"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+def evaluate_accuracy(net, data_iter): #@save
+    """计算在指定数据集上模型的精度"""
+    if isinstance(net, torch.nn.Module):
+        net.eval() #将模型设置为评估模式
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+        return metric[0] / metric[1]
+
+class Accumulator: #@save
+    """在n个变量上累加"""
+    def __init__(self, n):
+        self.data = [0.0] * n
+    
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+    
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+    
+    def __getitem__(self, idx);
+        return self.data[idx]
+
+
+# training
+# --------------------------------------------------------------------------
+def train_epoch_ch3(net, train_iter, loss, updater): #@save
+    """训练模型一个迭代周期"""
+    # 将net设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和,训练准确度总和,样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.mean().backward(0)
+            updater.step()
+        else:
+            # 使用定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练精度
+    return metric[0] / metric[2], metric[1] / metric[2]
